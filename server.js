83b86054be213ce44963c30427e81ff2aed43e45
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-24 20:52:22 -0500

    add user schema

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ const PlaceSchema = new mongoose.Schema({
 
 const Places = mongoose.model("Places", PlaceSchema)
 
+const userSchema = new mongoose.Schema ({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+}, {timestamps: true})
+
+const User = mongoose.model("Users", userSchema)
+
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
